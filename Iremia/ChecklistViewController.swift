f76cfda9880d19cbc9bcbf3b1b8284c39c7cb025
James <dangmjames@gmail.com>
2020-11-06 18:16:17 -0800

    First Checklist commit

--- Iremia/ChecklistViewController.swift
+++ Iremia/ChecklistViewController.swift
@@ -9,12 +9,77 @@ import UIKit
 
 class ChecklistViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        if !UserDefaults().bool(forKey: "setup") {
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        
+        updateTasks()
+    }
+    
+    func updateTasks() {
+        
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String{
+                tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
     }
    
+    @IBAction  func didTapAdd() {
+        let vc = storyboard?.instantiateViewController(identifier: "entry") as! EntryViewController
+        vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async{
+                self.updateTasks()
+            }
+        }
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 
+}
+
+extension ChecklistViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let vc = storyboard?.instantiateViewController(identifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+extension ChecklistViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
 }
 
