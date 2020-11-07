9465bc8edc7aec02b7dceb41a0ea36fa9bad6739
benshiao <shiaobenjamin@Gmail.com>
2020-11-06 17:42:24 -0800

    started wellness prototype

--- Iremia/WellnessViewController.swift
+++ Iremia/WellnessViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  WellnessViewController.swift
 //  Iremia
 //
 //  Copyright © 2020 Iremia. All rights reserved.
@@ -8,12 +8,38 @@
 import UIKit
 
 class WellnessViewController: UIViewController {
-
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var tasks = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //get current saved tasks
     }
 
+    @IBAction func didTapAdd() {
+        
+    }
 
 }
 
+extension WellnessViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        tableView.deselectRow(at: indexPath, animated : true)
+    }
+}
+
+extension WellnessViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection ssection: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+}
