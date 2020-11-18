adf7daa3841c00e956f588394cecde433ae5af10
Benjamin <52230442+benshiao@users.noreply.github.com>
2020-11-17 20:47:31 -0800

    Update WellnessViewController.swift
    Added 3 static wellnesss buttons/functionality

--- Iremia/WellnessViewController.swift
+++ Iremia/WellnessViewController.swift
@@ -53,6 +53,124 @@ class WellnessViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
     }
     
+    @IBAction func didTapMeals(_ sender: UIButton){
+        
+        //Navigates to the meals page
+        guard let vc = storyboard?.instantiateViewController(identifier: "MealsViewController") as? MealsViewController else {
+            return
+        }
+        //Set title for Meals wellness page
+        vc.title = "Meals"
+        
+        //When Meals repeat reminder is done and creates notification
+        vc.completion = {body, date in
+            DispatchQueue.main.async {
+                // Creating notification
+                let content = UNMutableNotificationContent()
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: true)
+                
+                let notif_id = "meals_id"
+                //delete old notification repeater
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [notif_id])
+                
+                //create new notifcation repeater
+                let request = UNNotificationRequest(identifier: notif_id, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
+        }
+        
+        // Pushes Add page on top
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTapBreaks(_ sender: UIButton){
+        
+        //Navigates to the Breaks page
+        guard let vc = storyboard?.instantiateViewController(identifier: "BreaksViewController") as? BreaksViewController else {
+            return
+        }
+        //Set title for Breaks wellness page
+        vc.title = "Breaks"
+        
+        //When Breaks repeat reminder is done and creates notification
+        vc.completion = {body, date in
+            DispatchQueue.main.async {
+                // Creating notification
+                let content = UNMutableNotificationContent()
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: true)
+                
+                let notif_id = "breaks_id"
+                //delete old notification repeater
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [notif_id])
+                
+                //create new notifcation repeater
+                let request = UNNotificationRequest(identifier: notif_id, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
+        }
+        
+        // Pushes Add page on top
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction func didTapSleep(_ sender: UIButton){
+        
+        //Navigates to the Sleep page
+        guard let vc = storyboard?.instantiateViewController(identifier: "SleepViewController") as? SleepViewController else {
+            return
+        }
+        //Set title for Sleep wellness page
+        vc.title = "Sleep"
+        
+        //When Sleep repeat reminder is done and creates notification
+        vc.completion = {body, date in
+            DispatchQueue.main.async {
+                // Creating notification
+                let content = UNMutableNotificationContent()
+                content.sound = .default
+                content.body = body
+                
+                let targetDate = date
+                let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate), repeats: true)
+                
+                let notif_id = "sleep_id"
+                //delete old notification repeater
+                UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [notif_id])
+                
+                //create new notifcation repeater
+                let request = UNNotificationRequest(identifier: notif_id, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+                    if error != nil {
+                        print("something went wrong")
+                    }
+                })
+            }
+        }
+        
+        // Pushes Add page on top
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
          
 
 }
