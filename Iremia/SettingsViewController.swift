cc08a4bbd42641ea4b4fde0f2c845c0567d0f256
benshiao <shiaobenjamin@Gmail.com>
2020-11-19 18:00:19 -0800

    merging all 3 backends together

--- Iremia/SettingsViewController.swift
+++ Iremia/SettingsViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,7 +25,24 @@ class SettingsViewController: UIViewController {
             UIApplication.shared.open(url!, options: [:], completionHandler: nil)
         }
     }
+}
 
+extension SettingsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("you tapped me!")
+    }
+    
+}
 
-
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = "Notification Settings"
+        
+        return cell
+    }
 }
