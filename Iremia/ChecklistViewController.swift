715734a8430d04d03b82172ed3904c926f338b38
MCSchulte <mcs228lady@gmail.com>
2020-11-20 18:36:48 -0800

    Gradient and colors added
    Gradient and other colors added to the checklistviewcontroller

--- Iremia/ChecklistViewController.swift
+++ Iremia/ChecklistViewController.swift
@@ -26,6 +26,8 @@ class ChecklistViewController: UIViewController {
     private let realm = try! Realm()
     //this array stores all the tasks on the database
     var taskList = [Task]()
+    //background gradient
+    @IBOutlet weak var backgroundGradientView: UIView!
     
     override func viewDidLoad() {
         //Ask for notifications permissions
@@ -37,6 +39,12 @@ class ChecklistViewController: UIViewController {
         taskList = realm.objects(Task.self).map({ $0 })
         table.delegate = self
         table.dataSource = self
+        table.separatorColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+        /*let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [#colorLiteral(red: 0.6792625189, green: 0.8248208165, blue: 0.7395270467, alpha: 1).cgColor, #colorLiteral(red: 0.3338187337, green: 0.3300850391, blue: 0.5314263105, alpha: 1).cgColor]
+        gradientLayer.shouldRasterize = true
+        backgroundGradientView.layer.addSublayer(gradientLayer)*/
         
     }
     
