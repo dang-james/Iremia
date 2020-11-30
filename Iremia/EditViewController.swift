f345892c17ac052bebbe79659dbbdcfd0fbbc45f
MCSchulte <mcs228lady@gmail.com>
2020-11-29 19:02:04 -0800

    Colors more readable
    Fixed the colors for add/edit tasks

--- Iremia/EditViewController.swift
+++ Iremia/EditViewController.swift
@@ -47,6 +47,10 @@ class EditViewController: UIViewController, UITextFieldDelegate {
         gradientLayer.colors = [#colorLiteral(red: 0.3338187337, green: 0.3300850391, blue: 0.5314263105, alpha: 1).cgColor, #colorLiteral(red: 0.6792625189, green: 0.8248208165, blue: 0.7395270467, alpha: 1).cgColor]
         gradientLayer.shouldRasterize = true
         backgroundGradientView.layer.addSublayer(gradientLayer)
+        titleField.attributedPlaceholder = NSAttributedString(string: "placeholder text", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)])
+        bodyField.attributedPlaceholder = NSAttributedString(string: "placeholder text", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)])
+        titleField.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        bodyField.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         self.view.addSubview(titleField)
         self.view.addSubview(bodyField)
         self.view.addSubview(datePicker)
