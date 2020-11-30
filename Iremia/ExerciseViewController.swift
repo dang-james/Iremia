dc5cdf6518c7b481ca48256da2582e679a74e919
NiceWow123 <68618884+NiceWow123@users.noreply.github.com>
2020-11-30 12:45:10 -0800

    Wellness Save Alert
    Added alert after clicking save on each of the wellness reminders

--- Iremia/ExerciseViewController.swift
+++ Iremia/ExerciseViewController.swift
@@ -63,6 +63,23 @@ class ExerciseViewController: UIViewController, UITextFieldDelegate {
     }
     
     @objc func didTapSave() {
+        let dialogMessage = UIAlertController(title: "Confirm", message: "Are you sure you want to save?", preferredStyle: .alert)
+        
+        //create ok button, moves to delete()
+        let ok = UIAlertAction(title: "Save", style: .default, handler: { (action) -> Void in
+             self.save()
+        })
+        //create cancel button
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
+        }
+        
+        dialogMessage.addAction(ok)
+        dialogMessage.addAction(cancel)
+        
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func save() {
         //return to previous page
         self.navigationController?.popToRootViewController(animated: true)
         
